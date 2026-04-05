25aa3c19d2d9675e98c83fe76bd656d6fe95616e
saniacodes <saniawaheed063>
2026-04-05 17:52:42 +0500

    Add Variables

---
+++ variables.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=3,b=9;
+    char e = 'Q';
+    float pi = 3.14;
+    double w = 5.67676;
+    bool is_true = true;
+    cout<<"The value of a is "<<a<<"\nThe value of b is "<<b;
+    cout<<"\nThe value of pi is "<<pi;
+    cout<<"\nThe value is "<<is_true;
+    return 0;
+}
