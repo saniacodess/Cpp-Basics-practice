1066c14897cb08b73ed0d06f819bf3e7b80b7ab2
saniacodes <saniawaheed063>
2026-04-05 21:24:05 +0500

    sum of two numbers

---
+++ input-output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter First number : ";
+    cin>>num1;
+    cout<<"Enter second number : ";
+    cin>>num2;
+    int sum = num1 + num2;
+    cout<<"The sum is : "<<sum;
+//     "<<" is called insertion operator.
+//     ">>" is called extraction operator.
+ }
